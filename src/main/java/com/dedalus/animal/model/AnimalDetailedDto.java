cf138dd54e3ad793d38fd8ff6e30c59b8f8bf7b7
Tobias Schmidt <tobias.schmidt@dedalus.eu>
2024-06-11 15:06:49 +0200

    adding detailedDto

---
+++ src/main/java/com/dedalus/animal/model/AnimalDetailedDto.java
@@ -0,0 +1,16 @@
+package com.dedalus.animal.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+@Builder
+public class AnimalDetailedDto {
+    private UUID uuid;
+    private String name;
+    private SpeciesType type;
+    private String comment;
+    private Boolean available;
+}
